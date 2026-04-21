c8154ff1d75e83148b14447b4899163519a339df
Matheus Fábio <matheusfabiors@gmail.com>
2026-04-21 02:07:24 -0300

    learning go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,69 @@
 package main
 
 import (
-	"fmt"
 	"go-til/internal/user"
 )
 
-func main() {
-		user := user.NewUser("Matheus fabio", 27)
-		fmt.Printf("Seu nome é: %s e sua idade é: %d\n", user.Name, user.Age)
+func main()  {
+	var users []user.User
+
+	user.AddUser(&users, user.NewUser("Matheus", 27, true))
+
+	user.ListUsers(users)
 }
+
+
+// "go-til/internal/user"
+
+// type User struct {
+// 	name string
+// 	age int
+// }
+
+// func (u User) IsAdult() bool {
+// 	return u.age >= 18
+// }
+
+
+// 	userMap := map[string]int{
+//     "Matheus": 25,
+// }
+
+// 	fmt.Println(userMap)
+	// nums := []int{1, 2, 3}
+	// nums = append(nums, 4)
+
+	// fmt.Print(nums)
+
+	// user := User{
+	// 	name: "Matheus",
+	// 	age: 27,
+	// }
+
+	// fmt.Println(user.IsAdult())
+	// user := User{name: "Matheus Fabio", age: 27}
+	// fmt.Println("user:", user.name, user.age)
+
+
+	// for i := 1; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// age := 12
+
+	// if age >= 18 {
+	// 	fmt.Println("Ja pode ser preso kkkk")
+	// } else {
+	// 	fmt.Println("Jaja chega a sua hora")
+	// }
+// }
+
+
+
+// func main() {
+// 		user := user.NewUser("Matheus fabio", 27, "Authenticated")
+// 		fmt.Println("user:", user.Name, user.Age, user.Autenticated)
+// }
+
+
 
--- internal/user/userInfo.go
+++ internal/user/userInfo.go
@@ -3,8 +3,14 @@ package user
 type User struct {
 	Name string
 	Age  int
+	autenticated bool
 }
 
-func NewUser(name string, age int) User {
-	return User{Name: name, Age: age}
+func (u User) IsAdult() bool {
+    return u.Age >= 18
 }
+
+func NewUser(name string, age int, autenticated bool) User {
+    return User{Name: name, Age: age, autenticated: autenticated}
+}
+
